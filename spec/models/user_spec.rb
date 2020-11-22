121b2cbb655282cb9b65977e7919bd6d2a55e50d
ikedayukinari <nari.1985.0201@gmail.com>
2020-11-22 11:49:55 +0900

    テスコードの記述を追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -55,6 +55,12 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is invalid')
       end
+      it 'passwordが半角英字だけでは登録できない' do
+        @user.password = 'aaaaa'
+        @user.password_confirmation = 'aaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is invalid')
+      end
       it 'passwordが存在してもpassword_confirmationが空では登録できない' do
         @user.password_confirmation = ''
         @user.valid?
