ce36eeb3d4c353a52ff8a0a6a08571852caca542
ikedayukinari <nari.1985.0201@gmail.com>
2020-11-23 18:54:33 +0900

    商品出品ページへの遷移、出品フォーム入力の実装

---
+++ app/models/item.rb
@@ -0,0 +1,27 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions  # ジャンルを選ぶ時のクリックする側
+  belongs_to :category, :condition, :shipping_charge, :days_to_ship, :prefecture
+  belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id
+    validates :condition_id
+    validates :shipping_charges_id
+    validates :prefectures_id
+    validates :days_to_ship_id
+    validates :selling_price, format: { with: /\A[0-9]+\z/ }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
+
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :shipping_charges_id, numericality: { other_than: 1 }
+  validates :days_to_ship_id, numericality: { other_than: 1 }
+  validates :prefectures_id, numericality: { other_than: 1 }
+end
+
+# include ActiveHash::Associations  ジャンルの中身のモデルにつける？
+# has_many :
