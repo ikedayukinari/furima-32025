b0d9cb756f3abb4f0e180cc6e9e0d24811cdff27
ikedayukinari <nari.1985.0201@gmail.com>
2020-11-24 13:21:36 +0900

    商品一覧機能 (修正)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_charges_id, :prefectures_id, :days_to_ship_id, :selling_price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :days_to_ship_id, :selling_price).merge(user_id: current_user.id)
   end
 end
--- db/migrate/20201122083815_create_items.rb
+++ db/migrate/20201122083815_create_items.rb
@@ -5,8 +5,8 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.text    :description,           null: false
       t.integer :category_id,           null: false
       t.integer :condition_id,          null: false
-      t.integer :shipping_charges_id,   null: false
-      t.integer :prefectures_id,        null: false
+      t.integer :shipping_charge_id,   null: false
+      t.integer :prefecture_id,        null: false
       t.integer :days_to_ship_id,       null: false
       t.integer :selling_price,         null: false
       t.references :user,               null: false, foreign_key: true
