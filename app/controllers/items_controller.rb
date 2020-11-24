f1322e6c4ded1d996f054bd993c7a1facb2516a0
ikedayukinari <nari.1985.0201@gmail.com>
2020-11-24 10:26:00 +0900

    出品商品の表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
+    @items = Item.all.order("created_at DESC").limit(3)
   end
 
   def new
